f2c7ea83eeae84cb43677c695ad6a477b7cb7c95
Jesse Gibson <JesseTheGibson@gmail.com>
2026-03-08 16:17:58 -0400

    Format all files with prettier/treefmt
    I already have my editor configured to do this, but I'm experimenting
    with new tools to improve formatting and pre-commit validation. This is
    a safe repo where I can try them.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+
+    let
+      inherit (nixpkgs) lib;
+
+      eachSystem = lib.flip lib.mapAttrs (
+        lib.genAttrs (import systems) (system: nixpkgs.legacyPackages.${system})
+      );
+    in
+
+    {
+      devShell = eachSystem (
+        system: pkgs:
+        pkgs.mkShell {
+          packages = [
+            pkgs.treefmt
+            pkgs.prettier
+          ];
+        }
+      );
+    };
+}
